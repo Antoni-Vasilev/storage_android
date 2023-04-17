687f4eb3ce16165051773f2c85c56c2325233d5d
TONI\antoni <antonivasilev15@gmail.com>
2023-04-17 23:37:57 +0300

    - 17.04.2023 | 23:37

--- app/src/main/java/com/storage/activity/ForgotPasswordActivity.java
+++ app/src/main/java/com/storage/activity/ForgotPasswordActivity.java
@@ -24,7 +24,7 @@ public class ForgotPasswordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
 
-        setTransparentStatusBar();
+//        setTransparentStatusBar();
         init();
     }
 
--- app/src/main/java/com/storage/activity/RegisterActivity.java
+++ app/src/main/java/com/storage/activity/RegisterActivity.java
@@ -15,7 +15,7 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        setTransparentStatusBar();
+//        setTransparentStatusBar();
     }
 
     private void setTransparentStatusBar() {
